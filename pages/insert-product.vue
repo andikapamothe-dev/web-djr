c09f89f81a9082713d8579aa411d1ab743b3f30b
andikapamothe-dev <pamothe.andika90@gmail.com>
2021-06-22 14:39:05 +0800

    initial

---
+++ pages/insert-product.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script>
+    export async function createUser(data) {
+    const response = await fetch('http://165.22.61.192:9911', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+      })
+    return await response.json();
+}
+</script>
